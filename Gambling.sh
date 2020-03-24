3c039c3d3f201e28cae5a3371549b74e61f81f44
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-23 23:19:50 -0700

    Use case 3

--- Gambling.sh
+++ Gambling.sh
@@ -6,6 +6,8 @@ for ((i=1;i<=$days;i++))
 do
 	stake=100
 	betAmount=1 
-	[ $((RANDOM%2)) -eq 1 ] && stake=$((stake+betAmount)) || stake=$((stake-betAmount))
-	
+	while [ $stake -gt 50 -a $stake -lt 150  ]
+	do
+		[ $((RANDOM%2)) -eq 1 ] && stake=$((stake+betAmount)) || stake=$((stake-betAmount))
+	done
 done
