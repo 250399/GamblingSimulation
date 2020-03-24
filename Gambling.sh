1c4917fc21f3b46783ba8564d1fa1819c59cf1a2
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-23 23:39:32 -0700

    Use case 3

--- Gambling.sh
+++ Gambling.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
 
-days=$((RANDOM%100))
+totalWinLoss () {
+	total=0
+	for i in ${arr[@]}
+	do
+		total=$((total+i))
+	done
+	totalStake=$((days*100))
+	if [ $total -lt $totalStake ]
+	then
+		echo "Total loss after 20 days :"$((totalStake-total))
+	elif [ $total -gt $totalStake ]
+	then
+		echo "Total winning after 20 days :"$((total-totalStake))
+	else
+		echo "No win No loss"
+	fi
+}
+
+days=20
 for ((i=1;i<=$days;i++))
 do
 	stake=100
@@ -10,4 +28,8 @@ do
 	do
 		[ $((RANDOM%2)) -eq 1 ] && stake=$((stake+betAmount)) || stake=$((stake-betAmount))
 	done
+	arr[$i]=$stake
 done
+
+
+totalWinLoss
